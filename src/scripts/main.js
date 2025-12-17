50e6b1e8c0549d83541a0e95f563068140e5477a
Andrey <kagamlykas@gmail.com>
2025-12-17 19:56:25 +0200

    Solved task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,34 @@
 'use strict';
 
-// write code here
+const table = document.querySelector('table');
+
+if (table) {
+  const headers = table.querySelector('thead');
+  const rows = [...table.querySelector('tbody').querySelectorAll('tr')];
+
+  if (headers && rows) {
+    headers.addEventListener('click', (ev) => {
+      const key = ev.target.closest('th');
+      const colNumber = [...headers.firstElementChild.children].indexOf(key);
+
+      if (['Salary', 'Age'].includes(key.textContent)) {
+        rows.sort(
+          (a, b) =>
+            parseCellToNumber(a, colNumber) - parseCellToNumber(b, colNumber),
+        );
+      } else {
+        rows.sort((a, b) => {
+          return a.children[colNumber].textContent.localeCompare(
+            b.children[colNumber].textContent,
+          );
+        });
+      }
+
+      table.querySelector('tbody').replaceChildren(...rows);
+    });
+  }
+}
+
+function parseCellToNumber(row, index) {
+  return parseInt(row.children[index].textContent.match(/\d+/g).join(''));
+}
